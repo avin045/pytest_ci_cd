86f1272e8e104678313b6ef2417a922215edcc2d
Avinash <avinashsekar045@gmail.com>
2023-10-24 21:34:00 +0530

    Added Pytest Condtition 1

--- test_main.py
+++ test_main.py
@@ -112,8 +112,8 @@ def test_column_match_sttm():
     count_ = 0
     sttm_table = session.table(name=config_dict["sttm_table"]).select("COLUMN NAME").collect();
     sttm_table_col_list = [cols[0].lower() for cols in sttm_table]
-    for column in target:
-        if column.lower() in sttm_table_col_list:
+    for column_ in target:
+        if column_.lower() in sttm_table_col_list:
             count_ += 1
     log_res = 'PASSED' if count_ == len(sttm_table_col_list) else 'NOT PASSED'
     logger.info(f"COLUMN MATCH WITH STTM : {log_res}")
